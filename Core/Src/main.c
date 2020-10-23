159f664ad7f99aaa04e5f9803fe87d15e98d49ae
sriniv61 <sriniv61@purdue.edu>
2020-10-23 19:02:51 -0400

    flash template code

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -72,6 +72,23 @@ static void MX_USART2_UART_Init(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+#define FLASH_USER_START_ADDR   ADDR_FLASH_SECTOR_2   /* Start @ of user Flash area */
+#define FLASH_USER_END_ADDR     ADDR_FLASH_SECTOR_11  +  GetSectorSize(ADDR_FLASH_SECTOR_11) -1 /* End @ of user Flash area : sector start address + sector size -1 */
+
+#define DATA_32                 ((uint32_t)0x87654321)
+
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+uint32_t FirstSector = 0, NbOfSectors = 0, Address = 0, EndAddr = 0;
+uint32_t SectorError = 0;
+__IO uint32_t data32 = 0 , MemoryProgramStatus = 0;
+
+/*Variable used for Erase procedure*/
+static FLASH_EraseInitTypeDef EraseInitStruct;
+
+/* Private function prototypes -----------------------------------------------*/
+static uint32_t GetSector(uint32_t Address);
+static uint32_t GetSectorSize(uint32_t Sector);
 
 /* USER CODE END 0 */
 
@@ -109,6 +126,77 @@ int main(void)
 
   My_SPI2_INIT(&hspi2);
   /* USER CODE BEGIN 2 */
+//  HAL_FLASH_Unlock();
+//  /* Erase the user Flash area
+//    (area defined by FLASH_USER_START_ADDR and FLASH_USER_END_ADDR) ***********/
+//  FirstSector = GetSector(FLASH_USER_START_ADDR);
+//  /* Get the number of sector to erase from 1st sector*/
+//  NbOfSectors = GetSector(FLASH_USER_END_ADDR) - FirstSector + 1;
+//
+//  /* Fill EraseInit structure*/
+//  EraseInitStruct.TypeErase = FLASH_TYPEERASE_SECTORS;
+//  EraseInitStruct.VoltageRange = FLASH_VOLTAGE_RANGE_3;
+//  EraseInitStruct.Sector = FirstSector;
+//  EraseInitStruct.NbSectors = NbOfSectors;
+//  if(HAL_FLASHEx_Erase(&EraseInitStruct, &SectorError) != HAL_OK)
+//  {
+//    /*
+//      Error occurred while sector erase.
+//      User can add here some code to deal with this error.
+//      SectorError will contain the faulty sector and then to know the code error on this sector,
+//      user can call function 'HAL_FLASH_GetError()'
+//    */
+//    while (1)
+//    {
+//    }
+//  }
+//  __HAL_FLASH_DATA_CACHE_DISABLE();
+//  __HAL_FLASH_INSTRUCTION_CACHE_DISABLE();
+//
+//  __HAL_FLASH_DATA_CACHE_RESET();
+//  __HAL_FLASH_INSTRUCTION_CACHE_RESET();
+//
+//  __HAL_FLASH_INSTRUCTION_CACHE_ENABLE();
+//  __HAL_FLASH_DATA_CACHE_ENABLE();
+//
+//
+//
+//  /* Program the user Flash area word by word
+//    (area defined by FLASH_USER_START_ADDR and FLASH_USER_END_ADDR) ***********/
+//
+//  Address = FLASH_USER_START_ADDR;
+  EndAddr = FLASH_USER_END_ADDR;
+//
+//  while (Address < EndAddr)
+//  {
+//    if (HAL_FLASH_Program(FLASH_TYPEPROGRAM_WORD, Address, DATA_32) == HAL_OK)
+//    {
+//      Address = Address + 4;
+//    }
+//    else
+//    {
+//      /* Error occurred while writing data in Flash memory.
+//         User can add here some code to deal with this error */
+//      while (1)
+//      {
+//      }
+//    }
+//  }
+//  HAL_FLASH_Lock();
+//  Address = FLASH_USER_START_ADDR;
+//  MemoryProgramStatus = 0;
+//
+//  while (Address < EndAddr)
+//  {
+//    data32 = *(__IO uint32_t*)Address;
+//
+//    if (data32 != DATA_32)
+//    {
+//      MemoryProgramStatus++;
+//    }
+//
+//    Address = Address + 4;
+//  }
 
   /* USER CODE END 2 */
 
@@ -119,7 +207,7 @@ int main(void)
   buttonPress button = NoPress;
 //  char str[13] = "hello world\r\n";
 //  HAL_UART_Transmit(&huart2, (uint8_t *)str, sizeof(str), 1);
-    uci_main(&hspi2, &huart2);
+    //uci_main(&hspi2, &huart2);
 
 	while (1)
 	{
@@ -131,9 +219,10 @@ int main(void)
 
 	  HAL_GPIO_TogglePin(GPIOD, ORANGE_TOPLED | BLUE_BOTTOMLED | GREEN_LEFTLED | RED_RIGHTled);
 	  HAL_GPIO_WritePin(GPIOE, SELECT_LED | START_LED | A_LED | B_LED, GPIO_PIN_SET);
-	  HAL_Delay(200);
+	  HAL_Delay(500);
 	  HAL_GPIO_TogglePin(GPIOD, ORANGE_TOPLED | BLUE_BOTTOMLED | GREEN_LEFTLED | RED_RIGHTled);
 	  HAL_GPIO_WritePin(GPIOE, SELECT_LED | START_LED | A_LED | B_LED, GPIO_PIN_RESET);
+	  HAL_Delay(500);
 
 	  /* USER CODE BEGIN 3 */
 	}
@@ -180,6 +269,11 @@ void SystemClock_Config(void)
   if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
   {
     Error_Handler();
+  }
+  if (HAL_GetREVID() == 0x1001)
+  {
+    /* Enable the Flash prefetch */
+    __HAL_FLASH_PREFETCH_BUFFER_ENABLE();
   }
 }
 
@@ -428,6 +522,80 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(MEMS_INT2_GPIO_Port, &GPIO_InitStruct);
 
+}
+static uint32_t GetSector(uint32_t Address)
+{
+  uint32_t sector = 0;
+
+  if((Address < ADDR_FLASH_SECTOR_1) && (Address >= ADDR_FLASH_SECTOR_0))
+  {
+    sector = FLASH_SECTOR_0;
+  }
+  else if((Address < ADDR_FLASH_SECTOR_2) && (Address >= ADDR_FLASH_SECTOR_1))
+  {
+    sector = FLASH_SECTOR_1;
+  }
+  else if((Address < ADDR_FLASH_SECTOR_3) && (Address >= ADDR_FLASH_SECTOR_2))
+  {
+    sector = FLASH_SECTOR_2;
+  }
+  else if((Address < ADDR_FLASH_SECTOR_4) && (Address >= ADDR_FLASH_SECTOR_3))
+  {
+    sector = FLASH_SECTOR_3;
+  }
+  else if((Address < ADDR_FLASH_SECTOR_5) && (Address >= ADDR_FLASH_SECTOR_4))
+  {
+    sector = FLASH_SECTOR_4;
+  }
+  else if((Address < ADDR_FLASH_SECTOR_6) && (Address >= ADDR_FLASH_SECTOR_5))
+  {
+    sector = FLASH_SECTOR_5;
+  }
+  else if((Address < ADDR_FLASH_SECTOR_7) && (Address >= ADDR_FLASH_SECTOR_6))
+  {
+    sector = FLASH_SECTOR_6;
+  }
+  else if((Address < ADDR_FLASH_SECTOR_8) && (Address >= ADDR_FLASH_SECTOR_7))
+  {
+    sector = FLASH_SECTOR_7;
+  }
+  else if((Address < ADDR_FLASH_SECTOR_9) && (Address >= ADDR_FLASH_SECTOR_8))
+  {
+    sector = FLASH_SECTOR_8;
+  }
+  else if((Address < ADDR_FLASH_SECTOR_10) && (Address >= ADDR_FLASH_SECTOR_9))
+  {
+    sector = FLASH_SECTOR_9;
+  }
+  else if((Address < ADDR_FLASH_SECTOR_11) && (Address >= ADDR_FLASH_SECTOR_10))
+  {
+    sector = FLASH_SECTOR_10;
+  }
+  else /* (Address < FLASH_END_ADDR) && (Address >= ADDR_FLASH_SECTOR_11) */
+  {
+    sector = FLASH_SECTOR_11;
+  }
+
+  return sector;
+}
+
+static uint32_t GetSectorSize(uint32_t Sector)
+{
+uint32_t sectorsize = 0x00;
+
+if((Sector == FLASH_SECTOR_0) || (Sector == FLASH_SECTOR_1) || (Sector == FLASH_SECTOR_2) || (Sector == FLASH_SECTOR_3))
+{
+  sectorsize = 16 * 1024;
+}
+else if(Sector == FLASH_SECTOR_4)
+{
+  sectorsize = 64 * 1024;
+}
+else
+{
+  sectorsize = 128 * 1024;
+}
+return sectorsize;
 }
 
 /* USER CODE BEGIN 4 */
